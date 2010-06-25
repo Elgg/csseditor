2ece94624009a8f4afa25ac011dc8e932ed8f4c5
Brett Profitt <brettp@36083f99-b078-4883-b0ff-0f9b5a30f544>
2010-06-25 18:45:05 +0000

    Added language strings for admin:appearance:customlogo and customcss.
    git-svn-id: http://code.elgg.org@6568 36083f99-b078-4883-b0ff-0f9b5a30f544

--- languages/en.php
+++ languages/en.php
@@ -1,41 +1,43 @@
 <?php
 
-	$english = array(
+$english = array(
+	/**
+	* Appearance
+	*/
 	
-		/**
-		 * Appearance
-		 */
+	'csseditor' => "Custom CSS",
+	'csseditor:warning' => "You can use the following form to edit a few of the css elements of your Elgg site. If you are not sure about editing CSS, then we recommend you leave this section alone. ",
+	'csseditor:posted' => "Your custom CSS settings have been saved.",
+	'csseditor:pagebackground' => "Page background",
+	'csseditor:headerbackground' => "Header background",
+	'csseditor:backgroundcolor' => "Background color (click in the field to access a color picker)",
+	'csseditor:backgroundimage' => "Background image (insert full url to image)",
+	'csseditor:headers' => "Headers (h1, h2 etc)",
+	'csseditor:links' => "Links",
+	'csseditor:paragraph' => "Paragraph tags",
+	'csseditor:ownstyle' => "Overwrite existing CSS classes and ids",
+	'csseditor:activate' => "Activate",
+	'csseditor:tileimage' => "Tile image",
+	'csseditor:transparentback' => "Make the header background transparent. This will ensure no background images appear and the page background displays instead. Warning - selecting this option will nullify any other header image options",
+	'csseditor:activatemessage' => "If you do not want your custom css to display at this time select 'no' below.",
+	'csseditor:save' => "Apply changes",
+	'item:object:csseditor' => "Custom CSS themes created",
 	
-		 'csseditor' => "Custom CSS",
-		 'csseditor:warning' => "You can use the following form to edit a few of the css elements of your Elgg site. If you are not sure about editing CSS, then we recommend you leave this section alone. ",
-		 'csseditor:posted' => "Your custom CSS settings have been saved.",
-		 'csseditor:pagebackground' => "Page background",
-		 'csseditor:headerbackground' => "Header background",
-		 'csseditor:backgroundcolor' => "Background color (click in the field to access a color picker)",
-		 'csseditor:backgroundimage' => "Background image (insert full url to image)",
-		 'csseditor:headers' => "Headers (h1, h2 etc)",
-		 'csseditor:links' => "Links",
-		 'csseditor:paragraph' => "Paragraph tags",
-		 'csseditor:ownstyle' => "Overwrite existing CSS classes and ids",
-		 'csseditor:activate' => "Activate",
-		 'csseditor:tileimage' => "Tile image",
-		 'csseditor:transparentback' => "Make the header background transparent. This will ensure no background images appear and the page background displays instead. Warning - selecting this option will nullify any other header image options",
-		 'csseditor:activatemessage' => "If you do not want your custom css to display at this time select 'no' below.",
-		 'csseditor:save' => "Apply changes",
-		 'item:object:csseditor' => "Custom CSS themes created",
+	'customlogo' => "Upload site logo",
+	
+	'customlogo:blurb' => "You can upload a custom logo for your site here.<br />(Images larger than 300 x 60px will be scaled down).",
+	
+	'customlogo:file' => "Upload your site logo",
+	'customlogo:delete' => 'Click here to remove your logo',
+	
+	'customlogo:success' => 'Your logo was successfully uploaded.',
+	'customlogo:failure' => 'Your logo could not be uploaded. Please check it was saved in a valid web image format and try again.',
+	
+	'csseditor:disabled_by_theme' => 'Custom CSS has been disabled by %s.  Visit the <a href="%s">CSS Editor settings page</a> to re-enabled.',
 
-		 'customlogo' => "Upload site logo",
-	
-		 'customlogo:blurb' => "You can upload a custom logo for your site here.<br />(Images larger than 300 x 60px will be scaled down).",
-	
-		 'customlogo:file' => "Upload your site logo",
-		 'customlogo:delete' => 'Click here to remove your logo',
-	
-		 'customlogo:success' => 'Your logo was successfully uploaded.',
-		 'customlogo:failure' => 'Your logo could not be uploaded. Please check it was saved in a valid web image format and try again.',
+	'admin:appearance:customlogo' => 'Custom Logo',
+	'admin:appearance:customcss' => 'Custom CSS'
 
-		 'csseditor:disabled_by_theme' => 'Custom CSS has been disabled by %s.  Visit the <a href="%s">CSS Editor settings page</a> to re-enabled.',
-	
-	);
-					
-	add_translation("en",$english);
+);
+
+add_translation("en",$english);
